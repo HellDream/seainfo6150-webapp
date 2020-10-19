f252f7e5f3b6e9017f5c386e317fca0182288be3
Zhenyu Ye <yzy673598755@gmail.com>
2020-10-18 20:30:33 -0700

    react app assignment

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import ArticleListItem from '../ArticleListItem/ArticleListItem';
 
 const ArticleList = props => {
+  if(props.articles===undefined||props.artiles===null){
+    return <h1>"You have no data!"</h1>
+  }
     return (
       <ul>
         {props.articles.map(article => (
